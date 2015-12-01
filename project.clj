5136501cf513060bfce9f6bca51705bf97c617ac
Marco Schneider <marco.schneider@active-group.de>
2015-12-01 10:25:33 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,5 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.xerial/sqlite-jdbc "3.8.11"]
                  [active-clojure "0.11.0"]]
-  :plugins [[lein-cloverage "1.0.6"]])
+  :plugins [[lein-cloverage "1.0.6"]
+            [lein-kibit "0.1.2"]])
