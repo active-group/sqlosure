63b1c114e869d7f0fdad13441e0402d06a592433
Marco Schneider <marco.schneider@active-group.de>
2015-12-14 14:13:25 +0100

    Added postgres dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/java.jdbc "0.4.2"]
-                 [org.xerial/sqlite-jdbc "3.8.11"]
+                 ;; uncomment only one
+                 ;; [org.xerial/sqlite-jdbc "3.8.11"]
+                 ;; [org.postgresql/postgresql "9.4-1206-jdbc41"]
                  [active-clojure "0.11.0"]]
   :plugins [[lein-cloverage "1.0.6"]
             [lein-kibit "0.1.2"]])
