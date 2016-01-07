88b09e7e9f17b4649051d7be80c773f915afa36a
Marco Schneider <marco.schneider.89@googlemail.com>
2016-01-07 18:36:54 +0100

    Added imports, shortened interop calls.

--- src/sqlosure/time.clj
+++ src/sqlosure/time.clj
@@ -1,7 +1,8 @@
 (ns sqlosure.time
   (:require [sqlosure.type :refer [date? timestamp?]]
             [active.clojure.condition :refer [assertion-violation]])
-  (:import [java.time LocalDate LocalDateTime]))
+  (:import [java.time LocalDate LocalDateTime]
+           [java.sql Date Timestamp]))
 
 (defn make-date
   "Wrapper around some common 'constructor' calls of LocalDate."
@@ -22,14 +23,14 @@
   "Takes a date object produced by java.time.LocalDate and returns a
   java.sql.Date."
   [d]
-  (if (instance? java.time.LocalDate d)
+  (if (instance? LocalDate d)
     (java.sql.Date/valueOf d)
     (assertion-violation 'to-sql-date "value of invalid date type" d)))
 
 (defn from-sql-date
   "Takes a date object of type java.sql.Date and returns a java.time.LocalDate."
   [sql-date]
-  (if (instance? java.sql.Date sql-date)
+  (if (instance? Date sql-date)
     (.toLocalDate sql-date)
     (assertion-violation 'from-sql-date "value of invalid date type" sql-date)))
 
@@ -37,14 +38,14 @@
   "Takes a date object produced by java.time.LocalDateTime and returns a
   java.sql.Timestamp."
   [t]
-  (if (instance? java.time.LocalDateTime t)
+  (if (instance? LocalDateTime t)
     (java.sql.Timestamp/valueOf t)
     (assertion-violation 'to-sql-timestamp "value of invalid timestamp type" t)))
 
 (defn from-sql-timestamp
   "Takes a date object of type java.sql.Timestamp and returns a java.time.LocalDateTime."
   [sql-timestamp]
-  (if (instance? java.sql.Timestamp sql-timestamp)
+  (if (instance? Timestamp sql-timestamp)
     (.toLocalDateTime sql-timestamp)
     (assertion-violation 'from-sql-timestamp "value of invalid timestamp type" sql-timestamp)))
 
