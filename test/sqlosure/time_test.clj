10d3d44b30a1c5c06f2fc97eeb4c025df920bebf
Marco Schneider <marco.schneider.89@googlemail.com>
2016-01-08 10:33:09 +0100

    Fixed time-test.

--- test/sqlosure/time_test.clj
+++ test/sqlosure/time_test.clj
@@ -6,12 +6,12 @@
 (def t1 (java.time.LocalDateTime/of 1989 10 31 0 0))
 
 (deftest make-date-test
-  (is (= (sqlosure.type/date? (make-date))))
+  (is (sqlosure.type/date? (make-date)))
   (is (= (make-date 1989 10 31) d1))
   (is (thrown? Exception (make-date :not-a-valid-date))))
 
 (deftest make-timestamp-test
-  (is (= (sqlosure.type/timestamp? (make-timestamp))))
+  (is (sqlosure.type/timestamp? (make-timestamp)))
   (is (= (make-timestamp 1989 10 31 0 0)
          (java.time.LocalDateTime/of 1989 10 31 0 0)))
   (is (= (make-timestamp 1989 10 31 0 0 0)
