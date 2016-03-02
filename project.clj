6b323888f8e114cd9243e9bf8cf6692c6914ed03
Mike Sperber <sperber@deinprogramm.de>
2016-03-02 11:25:11 +0100

    Inject humane-test-output, active.clojure.condition-hooks into REPL.

--- project.clj
+++ project.clj
@@ -15,7 +15,14 @@
 
   :profiles {:test {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
                     :injections [(require 'pjstadig.humane-test-output)
-                                 (pjstadig.humane-test-output/activate!)]}}
+                                 (pjstadig.humane-test-output/activate!)]}
+             :repl {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+                    :injections [(require 'pjstadig.humane-test-output)
+                                 (pjstadig.humane-test-output/activate!)
+                                 (require 'active.clojure.condition-hooks)
+                                 (active.clojure.condition-hooks/activate-clojure-test!)]}}
+
+  :repl-options {:caught active.clojure.condition-hooks/repl-caught}
 
   ;; Use `lein cover` to run cloverage. Reason: cloverage is not happy with lets
   ;; in monadic code. cover excludes the files that use
