5faa7288db0097948f1202375a1bc908620d52c2
Mike Sperber <sperber@deinprogramm.de>
2016-03-09 14:18:25 +0100

    Add description, license statement.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sqlosure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Compositional relational queries."
+  :url "https://github.com/active-group/sqlosure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
