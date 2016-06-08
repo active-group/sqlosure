dd8e7770455f403e0f279f3822f9667b2c188b16
Marco Schneider <marco.schneider.89@googlemail.com>
2016-06-08 12:27:32 +0200

    Added new utility functions.

--- src/sqlosure/utils.clj
+++ src/sqlosure/utils.clj
@@ -9,3 +9,13 @@
 (defn zip
   [xs ys]
   (mapv (fn [k v] [k v]) xs ys))
+
+(defn count=
+  "Are the lengths of `xs` and `ys` equal?"
+  [xs ys]
+  (= (count xs) (count ys)))
+
+(defn and?
+  "`and?` returns the conjunction of a seq of boolean(ish) values."
+  [bs]
+  (every? true? bs))
